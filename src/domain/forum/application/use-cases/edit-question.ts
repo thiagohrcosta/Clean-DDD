f25eaaf79721156d5955afd1c60d3c4eafe60b43
Thiagohrcosta <thiagohrcosta86@gmail.com>
2024-01-07 07:29:29 -0300

    Adding edit question

---
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -0,0 +1,39 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface EditQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  title: string
+  content: string
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface EditQuestionUseCaseResponse {}
+
+export class EditQuestionUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    authorId,
+    questionId,
+    title,
+    content,
+  }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    question.title = title
+    question.content = content
+
+    await this.questionsRepository.save(question)
+
+    return {}
+  }
+}
